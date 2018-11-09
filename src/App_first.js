d87d85d4c7c995eeb8dbbb3c7b7109091d1ed49b
MikeKowalsky <mkowalewski83@gmail.com>
2018-11-09 16:52:16 +0100

    creating second design

--- src/App_first.js
+++ src/App_first.js
@@ -13,34 +13,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-top-menu">
-          <i class="material-icons md-48">account_circle</i>
-          <i class="material-icons md-48">menu</i>
-        </div>
+
         <img src={ logo } className="App-logo" alt="logo"/>
         <p className="App-intro">
           Find your perfect trip, designed by insiders who know and love their cities.
         </p>
+        <p className="App-start-browsing">
+          Start browsing
+        </p>
         <Link to="/cities">
           <img src={ arrow } className="App-arrow" alt="arrow" />
         </Link>
-        <div className="App-call-to-action">
-          <p>
-            Popular MYtineraries
-          </p>
-        </div>
+        <p className="App-call-to-action">
+          Want to build your own MYtinerary?
+        </p>
 
-        <div className="App-slider">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-
-        {/* <div className="App-links">
+        <div className="App-links">
           <Link to="/login"><p>Log in</p></Link>
           <Link to="/signup"><p>Create Account</p></Link>
-        </div> */}
+        </div>
 
       </div>
     );
