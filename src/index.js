3d9b4dd81c5262eb46cf77f9160eab56c1e996c4
MikeKowalsky <mkowalewski83@gmail.com>
2018-11-28 21:50:48 +0100

    fetched data and present list of cities in Cities component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { Cities } from './components/Cities'
-import { Login } from './components/Login'
-import { SignUp } from './components/SignUp'
+import Cities from "./components/Cities";
+import { Login } from "./components/Login";
+import { SignUp } from "./components/SignUp";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
-
-
+import * as serviceWorker from "./serviceWorker";
 
 const routing = (
-    <Router>
-        <div>
-            <Route exact path="/" component={App} />
-            <Route path="/cities" component={Cities} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-        </div>
-    </Router>
-)
-ReactDOM.render(routing, document.getElementById('root'))
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/cities" component={Cities} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </div>
+  </Router>
+);
+ReactDOM.render(routing, document.getElementById("root"));
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
