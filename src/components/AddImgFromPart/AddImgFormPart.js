af92f3715e9a54b196526fe42604b5da3b7879ea
MikeKowalsky <mkowalewski83@gmail.com>
2019-02-25 19:02:42 +0100

    added link to signup from login component

--- src/components/AddImgFromPart/AddImgFormPart.js
+++ src/components/AddImgFromPart/AddImgFormPart.js
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const AddImgFormPart = ({ onChangeActivity, number }) => {
-  return (
-    <div className="form-control">
-      <label htmlFor="imgName">Activity no {number} - name:</label>
-      <input
-        type="text"
-        name={`${number}_name`}
-        minLength="3"
-        maxLength="20"
-        onChange={onChangeActivity}
-      />
-      <label htmlFor="imgUrl">Activity no {number} - url:</label>
-      <input
-        type="text"
-        name={`${number}_url`}
-        minLength="3"
-        maxLength="60"
-        onChange={onChangeActivity}
-      />
-    </div>
-  );
+class AddImgFormPart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activity: {
+        name: "",
+        url: ""
+      }
+    };
+  }
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div className="form-control frame">
+        <label htmlFor="imgName">Activity no {number} - name:</label>
+        <input
+          type="text"
+          name="name"
+          minLength="3"
+          maxLength="20"
+          onChange={this.onChange}
+          value={this.state.activity.name}
+        />
+        <label htmlFor="imgUrl">Activity no {number} - url:</label>
+        <input
+          type="text"
+          name="url"
+          minLength="3"
+          maxLength="60"
+          onChange={this.onChange}
+          value={this.state.activity.url}
+        />
+      </div>
+    );
+  }
+}
+
+AddImgFormPart.propTypes = {
+  givBackActivity: PropTypes.func.isRequired,
+  number: PropTypes.number.isRequired
 };
-
-// propTypes
 
 export default AddImgFormPart;
